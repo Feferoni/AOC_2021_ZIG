d4aee7413974e763e6e518fd1147a13d242251a0
Olle <ollelundmark89@gmail.com>
2024-09-28 15:16:59 +0200

    Day1

---
+++ src/day1.zig
@@ -0,0 +1,78 @@
+const std = @import("std");
+const readFile = @import("readFile.zig");
+
+fn sumRange(slice: []const u32, start: usize, end: usize) u32 {
+    var sum: u32 = 0;
+    for (slice[start..end]) |num| {
+        sum += num;
+    }
+    return sum;
+}
+
+fn convertStringsToNumbers(strings: std.ArrayList([]const u8)) ![]u32 {
+    var numbers = std.ArrayList(u32).init(std.heap.page_allocator);
+    errdefer numbers.deinit();
+
+    for (strings.items) |line| {
+        const number = try std.fmt.parseInt(u32, line, 10);
+        try numbers.append(number);
+    }
+
+    return numbers.toOwnedSlice();
+}
+
+fn getNumberOfIncreases1(depths: []const u32) u32 {
+    var count: u32 = 0;
+
+    for (depths[1..], 0..) |curr, i| {
+        const prev = depths[i];
+        if (curr > prev) count += 1;
+    }
+
+    return count;
+}
+
+fn getNumberOfIncreases2(depths: []const u32, window_size: u32) u32 {
+    var count: u32 = 0;
+
+    var i: usize = window_size + 1;
+    var prev: u32 = sumRange(depths, 0, window_size);
+
+    while (i <= depths.len) : (i += 1) {
+        const curr = sumRange(depths, i - window_size, i);
+        if (curr > prev) count += 1;
+        prev = curr;
+    }
+
+    return count;
+}
+
+pub fn part1() !void {
+    const lines = try readFile.getLinesFromFile("day1.txt");
+    defer lines.deinit();
+
+    const depthScan = try convertStringsToNumbers(lines);
+    defer std.heap.page_allocator.free(depthScan);
+
+    std.debug.print("part1 result: {}\n", .{getNumberOfIncreases1(depthScan)});
+}
+
+pub fn part2() !void {
+    const lines = try readFile.getLinesFromFile("day1.txt");
+    defer lines.deinit();
+
+    const depthScan = try convertStringsToNumbers(lines);
+    defer std.heap.page_allocator.free(depthScan);
+
+    std.debug.print("part2 result: {}\n", .{getNumberOfIncreases2(depthScan, 3)});
+}
+
+test "part1" {
+    const depthScan = [_]u32{ 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 };
+    try std.testing.expectEqual(7, getNumberOfIncreases1(&depthScan));
+}
+
+test "part2" {
+    const depthScan = [_]u32{ 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 };
+    try std.testing.expectEqual(5, getNumberOfIncreases2(&depthScan, 3));
+}
---
+++ src/readFile.zig
@@ -0,0 +1,29 @@
+const std = @import("std");
+
+pub fn getLinesFromFile(filename: []const u8) !std.ArrayList([]const u8) {
+    const prefix_path = "./input/";
+    var path_buffer: [std.fs.max_path_bytes]u8 = undefined;
+    const full_path = try std.fmt.bufPrint(&path_buffer, "{s}{s}", .{ prefix_path, filename });
+
+    const file = try std.fs.cwd().openFile(full_path, .{});
+    defer file.close();
+
+    var buf_reader = std.io.bufferedReader(file.reader());
+    var in_stream = buf_reader.reader();
+
+    var lines = std.ArrayList([]const u8).init(std.heap.page_allocator);
+    errdefer {
+        for (lines.items) |line| {
+            std.heap.page_allocator.free(line);
+        }
+        lines.deinit();
+    }
+
+    var buf: [1024]u8 = undefined;
+    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        const dup = try std.heap.page_allocator.dupe(u8, line);
+        try lines.append(dup);
+    }
+
+    return lines;
+}
